0de2f52823de3df1dd523c46a4360c5607b3c717
Ismael Trocha <ismaeltrocha@gmail.com>
2023-10-24 23:10:09 -0500

    Fixing email verification, all works

---
+++ src/main/java/com/register/login/domain/service/UserService.java
@@ -0,0 +1,20 @@
+package com.register.login.domain.service;
+
+import com.register.login.domain.entites.User;
+import com.register.login.domain.registration.RegistrationRequest;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+
+  List<User> getUser();
+
+  User registerUser(RegistrationRequest registrationRequest);
+
+  Optional<User> findByEmail(String email);
+
+  void saveUserVerificationToken(User theUser, String verificationToken);
+
+  String validateToken(String token);
+
+}
